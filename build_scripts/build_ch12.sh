b377a2bbb7876886c61eced4fba2151c42fddc2e
KANGWEII <pkangwei@outlook.com>
2025-03-17 22:03:24 +0800

    Chapter12: remove examples with issue

--- build_scripts/build_ch12.sh
+++ build_scripts/build_ch12.sh
@@ -4,21 +4,6 @@ START_DIR=${PWD%/*}
 LIBS_DIR=$START_DIR/env_scripts/libs
 
 #Chapter 12
-cd $START_DIR/Chapter12/caffe2
-if [ -d build ]; then
-    echo "Existing build folder found"
-    cd build/
-    rm -r *
-else
-    mkdir build
-    cd build/
-fi
-cmake -DCMAKE_INSTALL_PREFIX=$LIBS_DIR ..
-cmake --build . --target all
-
-# Please use the following command line to run the caffe2 sample:
-# LD_LIBRARY_PATH=$LIBS_DIR/sources/pytorch/third_party/ideep/mkl-dnn/external/mklml_lnx_2019.0.3.20190220/lib/ ./caffe2_load
-
 cd $START_DIR/Chapter12/dlib
 if [ -d build ]; then
     echo "Existing build folder found"
@@ -30,19 +15,3 @@ else
 fi
 cmake -DDLIB_PATH=$LIBS_DIR ..
 cmake --build . --target all
-
-cd $START_DIR/Chapter12/pytorch
-if [ -d build ]; then
-    echo "Existing build folder found"
-    cd build/
-    rm -r *
-else
-    mkdir build
-    cd build/
-fi
-cmake -DCMAKE_INSTALL_PREFIX=$LIBS_DIR ..
-cmake --build . --target all
-
-# Please use the following command line to run the pytorch sample:
-# LD_LIBRARY_PATH=$LIBS_DIR/sources/pytorch/third_party/ideep/mkl-dnn/external/mklml_lnx_2019.0.3.20190220/lib/ ./pytorch-save
-
